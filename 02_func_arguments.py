3f9b7b4b5edcf2bd52a8d7f9eb2f450a54b41ade
peddadabhavana <95434462+peddadabhavana@users.noreply.github.com>
2026-06-14 12:40:34 +0530

    Update 02_func_arguments.py

--- 02_func_arguments.py
+++ 02_func_arguments.py
@@ -7,3 +7,10 @@ def average(a,b,c):
 print(average(20,34,86))
 print(int(average(50,34,80)))
 print(int(average(1,2,3)))
+
+# output
+# PS C:\Users\bhava\OneDrive\Desktop\Python> & C:\Users\bhava\AppData\Local\Microsoft\WindowsApps\python3.12.exe c:/Users/bhava/OneDrive/Desktop/Python/functions/02_func_arguments.py
+# 82.66666666666667
+# 110
+# 4
+# PS C:\Users\bhava\OneDrive\Desktop\Python> 
